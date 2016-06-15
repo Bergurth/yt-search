e860452dd76c0e1210351a137e2620a6dea5d4e3
bergurth <bergurthssn@gmail.com>
2016-06-15 13:04:49 +0000

    index.html and js/yt_search.js along with some dependencies

---
+++ js/yt_search.js
@@ -0,0 +1,18 @@
+// yt_search.js
+
+
+$(function() {
+      $("#search-btn").click( function()
+           {
+           	 var search_info = $('#search-input').val();
+           	 // https://www.youtube.com/results?search_query=brad+pitt    for example
+           	 search_info = search_info.split(" ").join("+");
+
+           	 var yt_search_string = 'https://www.youtube.com/results?search_query=';
+           	 var search_string = yt_search_string + search_info;
+             //alert(search_string);
+             window.location.replace(search_string);
+
+           }
+      );
+});
